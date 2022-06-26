9e6ac66b3e2243d651e13b431eec33e719009bc5
metaspook <metaspook@gmail.com>
2022-06-26 18:34:42 +0600

    added tab button bar

--- lib/pages/products_page.dart
+++ lib/pages/products_page.dart
@@ -1,3 +1,5 @@
+import 'package:comfort_place/widgets/search_bar.dart';
+import 'package:comfort_place/widgets/tab_button_bar.dart';
 import 'package:flutter/material.dart';
 
 class ProductsPage extends StatelessWidget {
@@ -36,90 +38,29 @@ class ProductsPage extends StatelessWidget {
               shrinkWrap: true,
               children: [
                 Row(
-                  children: [
-                    Expanded(
-                      child: Container(
-                        padding: const EdgeInsets.all(12),
-                        alignment: Alignment.center,
-                        height: kToolbarHeight * .75,
-                        decoration: BoxDecoration(
-                          color: Colors.grey.shade300,
-                          borderRadius: BorderRadius.circular(5),
-                          // gradient: LinearGradient(
-                          //   begin: Alignment.topRight,
-                          //   end: Alignment.bottomLeft,
-                          //   colors: [
-                          //     Color(0xFFFCD1B7),
-                          //     Color(0xFFBFE0E8),
-                          //   ],
-                          // ),
-                        ),
-                        child: Row(
-                          children: [
-                            Icon(
-                              Icons.search,
-                              color: Colors.grey.shade600,
-                            ),
-                            const SizedBox(width: 5),
-                            Text(
-                              'Search here',
-                              style: TextStyle(
-                                color: Colors.grey.shade600,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    const SizedBox(width: 10),
-                    const Icon(Icons.more_rounded, size: 50),
-                    const SizedBox(width: 15),
+                  children: const [
+                    Expanded(child: SearchBar()),
+                    SizedBox(width: 10),
+                    Icon(Icons.more_rounded, size: 50),
+                    SizedBox(width: 15),
                   ],
                 ),
                 const SizedBox(height: 10),
+                const TabButtonBar(),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    ElevatedButton.icon(
-                      style: ElevatedButton.styleFrom(
-                        elevation: 20,
-                        onPrimary: Colors.grey.shade600,
-                        primary: Theme.of(context).scaffoldBackgroundColor,
-                        shadowColor: Colors.grey.shade200,
-                      ),
+                    const Text('Nearby Place'),
+                    // const Spacer(),
+                    TextButton(
                       onPressed: () {},
-                      icon: const Icon(
-                        Icons.house_outlined,
+                      child: Row(
+                        children: const [
+                          Text('Baton Rouge (LA)'),
+                          SizedBox(width: 15),
+                        ],
                       ),
-                      label: const Text('House'),
                     ),
-                    ElevatedButton.icon(
-                      style: ElevatedButton.styleFrom(
-                        elevation: 20,
-                        onPrimary: Colors.grey.shade600,
-                        primary: Theme.of(context).scaffoldBackgroundColor,
-                        shadowColor: Colors.grey.shade200,
-                      ),
-                      onPressed: () {},
-                      icon: const Icon(
-                        Icons.house_siding_sharp,
-                      ),
-                      label: const Text('Apartment'),
-                    ),
-                    ElevatedButton.icon(
-                      style: ElevatedButton.styleFrom(
-                        elevation: 20,
-                        onPrimary: Colors.grey.shade600,
-                        primary: Theme.of(context).scaffoldBackgroundColor,
-                        shadowColor: Colors.grey.shade200,
-                      ),
-                      onPressed: () {},
-                      icon: const Icon(
-                        Icons.hotel,
-                      ),
-                      label: const Text('Hotel'),
-                    ),
-                    const SizedBox(width: 15),
                   ],
                 ),
               ],
---
+++ lib/widgets/tab_button_bar.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class TabButtonBar extends StatelessWidget {
+  const TabButtonBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        ElevatedButton.icon(
+          style: ElevatedButton.styleFrom(
+            padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 20),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(7.5),
+            ),
+            elevation: 35,
+            onPrimary: Colors.grey.shade600,
+            primary: Theme.of(context).scaffoldBackgroundColor,
+            shadowColor: Colors.black54,
+          ),
+          onPressed: () {},
+          icon: const Icon(
+            FontAwesomeIcons.houseUser,
+            color: Colors.deepOrange,
+            size: 15,
+          ),
+          label: const Text('House'),
+        ),
+        ElevatedButton.icon(
+          style: ElevatedButton.styleFrom(
+            padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 20),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(7.5),
+            ),
+            elevation: 20,
+            onPrimary: Colors.grey.shade600,
+            primary: Theme.of(context).scaffoldBackgroundColor,
+            shadowColor: Colors.black54,
+          ),
+          onPressed: () {},
+          icon: const Icon(
+            FontAwesomeIcons.building,
+            color: Colors.blue,
+            size: 15,
+          ),
+          label: const Text('Apartment'),
+        ),
+        ElevatedButton.icon(
+          style: ElevatedButton.styleFrom(
+            padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 20),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(7.5),
+            ),
+            elevation: 20,
+            onPrimary: Colors.grey.shade600,
+            primary: Theme.of(context).scaffoldBackgroundColor,
+            shadowColor: Colors.black54,
+          ),
+          onPressed: () {},
+          icon: const Icon(
+            FontAwesomeIcons.hotel,
+            color: Colors.pink,
+            size: 15,
+          ),
+          label: const Text('Hotel'),
+        ),
+        const SizedBox(width: 0),
+      ],
+    );
+  }
+}
